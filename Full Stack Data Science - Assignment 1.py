8846d80fe936baad177a664bd1890d8f4f487db8
Heman6 <94130064+Heman6@users.noreply.github.com>
2022-10-29 02:48:50 +0530

    Add files via upload

---
+++ Full Stack Data Science - Assignment 1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Full Stack Data Science - Assignment 1
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+
+# Ans: * = Expressions
+#     'hello' = String
+#     -87.8 = integer
+#     - = value
+#     / = value
+#     + = value
+#     6 = integer
+
+# 2. What is the difference between string and variable?
+
+# Ans: Variables are symbols that we can use to store data in a program. We can think of them as an empty box that we fill with some data or value. Strings are data, so we can use them to fill up a variable.
+
+# 3.Describe three different data types
+
+# Ans: a) Numeric - Integer, complex, float
+#      b) Dictionary
+#      c) Boolean
+#      d) Set
+#      e) Sequence Type - Strings, List, Tuple
+
+# 4. What is an expression made up of? What do all expressions do?
+
+# Ans: An expression is a combination of operators and operands that is interpreted to produce some value. In any programming language, an expression is evaluated as per the precedence of its operators.
+
+# 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+# Ans: Statement is just a line of code which is in String format. Where experession is combintion of values and operators (= & 10, String = like spam)
+
+# 6. After running the following code, what does the variable bacon contain?
+#    bacon = 22
+#    bacon + 1
+# 
+
+# Ans: 23
+
+# 7. What should the values of the following two terms be?
+#    'spam' + 'spamspam'
+#    'spam' * 3
+# 
+
+# Ans: 'spam' + 'spamspam' = 'spamspamspam'
+
+# Ans: 'spam'*3 = 'spamspamspam'
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+
+# Ans: eggs is string and also doesn't match with any operator in python. Whereas 100 is an integer and the chances to get sync is high
+
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+# Ans:  int() , float() , and str( ) 
+
+# 10. Why does this expression cause an error? How can you fix it?
+#     'I have eaten ' + 99 + ' burritos.'
+# 
+
+# Ans: W = 'I have eaten 99 burritos'
+# 
+# The expression was causing and error because of the placement of the values, integers and string was wrong.
+
+# In[ ]:
+
+
+
+
